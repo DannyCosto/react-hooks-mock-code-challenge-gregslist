68f7f15a29a53669cc8f7ec18b27708ae2f71c5c
Danny <danny.costelloe@gmail.com>
2021-04-11 13:08:55 -0400

    Core+Advanced

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,25 +1,42 @@
 import React, { useEffect, useState } from "react";
 import ListingCard from "./ListingCard";
+import NewListingForm from "./NewListingForm";
 
-function ListingsContainer({search}) {
-  const [listings, setListings] = useState([])
-  
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
+
   useEffect(() => {
     fetch("http://localhost:6001/listings")
-    .then((r) =>r.json())
-    .then((listings) => setListings(listings));
+      .then((r) => r.json())
+      .then((listings) => setListings(listings));
   }, []);
 
   function handleDeleteListing(id) {
-    const updatedListingsArr = listings.filter(listing => listing.id !== id)
-    setListings(updatedListingsArr)
+    const updatedListingsArray = listings.filter(
+      (listing) => listing.id !== id
+    );
+    setListings(updatedListingsArray);
+  }
+
+  function handleAddListing(newListing) {
+    const updatedListingsArray = [newListing, ...listings];
+    setListings(updatedListingsArray);
   }
 
   const filteredListings = listings.filter((listing) =>{
     return(listing.description.toLowerCase().includes(search.toLowerCase()))
   }) 
+
+  const sortedListings = filteredListings.sort((listingA, listingB) => {
+    if (sortBy === "id") {
+      return listingA.Id - listingB.id;
+    } else {
+      return listingA.location.localeCompare(listingB.location)
+    }
+  });
   
-  const listingCards = filteredListings.map((listingObj) => {
+  const listingCards = sortedListings.map((listingObj) => {
     return <ListingCard 
     key ={listingObj.id} 
     listing ={listingObj}
@@ -28,6 +45,9 @@ function ListingsContainer({search}) {
   
   return (
     <main>
+      <NewListingForm onAddListing = {handleDeleteListing} />
+      <button onClick={()=> setSortBy("location")}>Sort By Location</button>
+      <button onClick={()=> setSortBy("id")}>Sort By Default</button>
       <ul className="cards">{listingCards}</ul>
     </main>
   );
